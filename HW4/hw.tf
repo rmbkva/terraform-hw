6371ecf37d00000a98fb0ab50cad14f751b7bd38
rmbkva <as.rmbkva@gmail.com>
2024-04-11 00:28:52 +0000

    added hw4

---
+++ HW4/hw.tf
@@ -0,0 +1,53 @@
+provider aws {
+    region = var.region 
+}
+
+  resource "aws_instance" "web" {
+    ami = var.ami_id
+  instance_type = var.type
+   availability_zone = var.availability_zone
+   key_name = var.key_name
+   count = var.instance_count
+  }
+
+  variable ami_id {
+    description = "Provided ami id"
+    default = ""
+    type = string 
+  }
+
+  variable type {
+    description = "Provided instance type "
+    default = ""
+    type = string 
+  }
+
+  variable region {
+    description = "Provided region  "
+    default = ""
+    type = string 
+  }
+
+    variable key_name {
+    description = "Provided key_name  "
+    default = "my-laptop-key"
+    type = string 
+  }
+
+    variable availability_zone {
+    description = "Provided AZ  "
+    default = ""
+    type = string 
+  }
+
+    variable instance_count {
+    description = "Provided count "
+    default = 1
+    type = number
+  }
+
+    variable ports {
+    description = "Provided ports "
+    default = [22 , 80, 443]
+    type = list(number)
+  }
---
+++ HW4/regions.tf
@@ -0,0 +1,24 @@
+#virginia 
+# region = "us-east-1"
+# ami_id = "ami-051f8a213df8bc089"
+# type = "t2.micro"
+#availability_zone = "us-east-1a" 
+
+#ohio
+# region = "us-east-2"
+# ami_id = "ami-0900fe555666598a2"
+# type = "t2.micro"
+#availability_zone = "us-east-2a"
+
+
+#california
+# region = "us-west-1"
+# ami_id = "ami-0b990d3cfca306617"
+# type = "t2.micro"
+#availability_zone = "us-west-1a"
+
+#oregon
+# region = "us-west-2"
+# ami_id = "ami-0395649fbe870727e"
+# type = "t2.micro"
+#availability_zone = "us-west-2a"
